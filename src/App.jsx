951ad7dcfb524b340f36970a27a12f3755786d77
Witee <xieweiting@admaster.com.cn>
2019-10-11 19:07:53 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Game from './containers/Game';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return <Game />;
+  }
+}
+
+export default App;
